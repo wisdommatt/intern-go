7993770ecf061b4c9098c07e79712579f0e6c47b
Wisdom Matthew <talk2wisdommatt@gmail.com>
2022-03-31 22:38:02 +0100

    feat(backend): add firestore client setup

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,10 @@ module github.com/wisdommatt/intern-go/backend
 go 1.16
 
 require (
+	cloud.google.com/go/firestore v1.6.1 // indirect
+	cloud.google.com/go/iam v0.3.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible
 	github.com/99designs/gqlgen v0.17.2
 	github.com/vektah/gqlparser/v2 v2.4.0
+	google.golang.org/api v0.74.0
 )
--- backend/server.go
+++ backend/server.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/wisdommatt/intern-go/backend/graph"
 	"github.com/wisdommatt/intern-go/backend/graph/generated"
+	"google.golang.org/api/option"
 )
 
 const defaultPort = "8080"
@@ -19,6 +23,9 @@ func main() {
 		port = defaultPort
 	}
 
+	firestoreClient := mustSetupFirestoreClient()
+	defer firestoreClient.Close()
+
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/graphql/playground", playground.Handler("GraphQL playground", "/graphql/query"))
@@ -26,4 +33,18 @@ func main() {
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func mustSetupFirestoreClient() *firestore.Client {
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("firebase-service-account.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return client
 }
